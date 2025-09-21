25ddd7562d2fa86714cd46a67d296fcb50021ee8
KonoLoserDa <thebestinsomniapage@gmail.com>
2025-09-21 21:17:41 +0200

    Initial commit

---
+++ src/client/java/com/musketeers/foolish_guns/FoolishGunsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.musketeers.foolish_guns;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FoolishGunsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
