8dc8b76df94f12aaa7701a3987898ac50b15a14e
gr00ze <vincenzop23@gmail.com>
2025-10-16 01:16:16 +0200

    Shooting on release

--- src/main/java/com/musketeers/foolish_guns/items/PrototypeGunItem.java
+++ src/main/java/com/musketeers/foolish_guns/items/PrototypeGunItem.java
@@ -6,8 +6,10 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
@@ -17,6 +19,7 @@ import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animatable.manager.AnimatableManager;
 import software.bernie.geckolib.animatable.processing.AnimationController;
+import software.bernie.geckolib.animatable.processing.AnimationTest;
 import software.bernie.geckolib.animation.PlayState;
 import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.util.GeckoLibUtil;
@@ -27,6 +30,11 @@ import java.util.function.Consumer;
 public class PrototypeGunItem extends Item implements GeoItem {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
     private static final RawAnimation ACTIVATE_ANIM = RawAnimation.begin().thenPlay("animation.gun_model.tesla");
+    private boolean canFire = false;
+    private boolean isLoading = false;
+    private ServerLevel currentLevel;
+    private Player currentPlayer;
+
     public PrototypeGunItem(Properties properties) {
 
         super(properties);
@@ -35,13 +43,43 @@ public class PrototypeGunItem extends Item implements GeoItem {
     }
 
     @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
-        controllerRegistrar.add(new AnimationController<>("Activation", 0, animTest -> PlayState.STOP).triggerableAnim("activate", ACTIVATE_ANIM)
-                .setCustomInstructionKeyframeHandler(event->{
-                    if(event.keyframeData().getInstructions().equals("fire")){
-                        this.shoot();
-                    }
-                }));
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+        /*
+        controllers.add(new AnimationController<>("Activation",0, animTest -> PlayState.STOP)
+                .triggerableAnim("activate", ACTIVATE_ANIM));
+                */
+        controllers.add(new AnimationController<>("MyController", 0, this::predicate)
+                .triggerableAnim("activate", ACTIVATE_ANIM)
+        );
+
+    }
+
+    private PlayState predicate(AnimationTest<PrototypeGunItem> animationTest){
+        AnimationController<PrototypeGunItem> controller = animationTest.controller();
+        var currAnim = controller.getCurrentRawAnimation();
+        if (currAnim == null) return PlayState.CONTINUE;
+
+
+        if (currAnim.getAnimationStages().get(0).animationName().equals("animation.gun_model.tesla")) {
+            // Se l'animazione è finita, chiama il metodo della classe
+            //System.out.println(controller.getAnimationState());
+            if (controller.getAnimationState() == AnimationController.State.STOPPED) {
+                //System.out.println(controller.getAnimationState());
+
+                //this.onAnimationEnd();
+            }
+        }
+        return PlayState.CONTINUE;
+    }
+
+    public void onAnimationEnd(){
+        if (!isLoading) return;
+        isLoading = false;
+        this.spawnParticles(currentLevel,currentPlayer);
+        this.hitEnemy(currentLevel,currentPlayer);
+
+        currentPlayer.playSound(SoundEvents.WARDEN_SONIC_BOOM, 8.0F, 15.0F);
 
     }
 
@@ -65,61 +103,70 @@ public class PrototypeGunItem extends Item implements GeoItem {
     }
 
     @Override
+    public boolean releaseUsing(ItemStack itemStack, Level level, LivingEntity livingEntity, int i) {
+        System.out.println("it is called");
+        onAnimationEnd();
+        AnimationController<?> controller = this.getAnimatableInstanceCache()
+                .getManagerForId(0)
+                .getAnimationControllers()
+                .get("MyController");
+
+        if (controller != null) controller.forceAnimationReset();
+        //player.getCooldowns().addCooldown(this, 40);
+        return super.releaseUsing(itemStack, level, livingEntity, i);
+    }
+
+    @Override
     public @NotNull InteractionResult use(Level level, Player player, InteractionHand hand) {
+        if (!(level instanceof ServerLevel serverLevel) || isLoading) return InteractionResult.FAIL;
+        isLoading = true;
 
-        if (level instanceof ServerLevel serverLevel){
-            //animation
-            triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(hand), serverLevel), "Activation", "activate");
+        this.currentLevel = serverLevel;
+        this.currentPlayer = player;
 
+        //animation
+        triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(hand), serverLevel), "MyController", "activate");
 
-            //particles
-            for (int i = 0; i < 100; i++) {
-                Vec3 scaled = player.getEyePosition().add(player.getLookAngle().scale(i));
-                serverLevel.sendParticles(ParticleTypes.SONIC_BOOM,scaled.x,scaled.y,scaled.z, 1,0,0,0,0);
+        player.startUsingItem(hand);
+        return InteractionResult.CONSUME;
+    }
 
-            }
-            //kill
-            Vec3 eyePos = player.getEyePosition(1.0F);
-            Vec3 look = player.getViewVector(1.0F);
-            double range = 50.0;
-            Vec3 end = eyePos.add(look.scale(range));
-            EntityHitResult entityHit = null;
-            double closestDistance = range;
-            for (Entity entity : level.getEntities(player, player.getBoundingBox().expandTowards(look.scale(range)).inflate(1.0))) {
-                if (!entity.isPickable() || entity == player) continue;
+    private void hitEnemy(ServerLevel level, Player player) {
+        Vec3 eyePos = player.getEyePosition(1.0F);
+        Vec3 look = player.getViewVector(1.0F);
+        double range = 50.0;
+        Vec3 end = eyePos.add(look.scale(range));
+        EntityHitResult entityHit = null;
+        double closestDistance = range;
+        for (Entity entity : level.getEntities(player, player.getBoundingBox().expandTowards(look.scale(range)).inflate(1.0))) {
+            if (!entity.isPickable() || entity == player) continue;
 
-                AABB box = entity.getBoundingBox().inflate(0.3D); // margine per entità piccole
-                Optional<Vec3> hit = box.clip(eyePos, end);
-                if (hit.isPresent()) {
-                    double dist = eyePos.distanceTo(hit.get());
-                    if (dist < closestDistance) {
-                        closestDistance = dist;
-                        entityHit = new EntityHitResult(entity, hit.get());
+            AABB box = entity.getBoundingBox().inflate(0.3D); // margine per entità piccole
+            Optional<Vec3> hit = box.clip(eyePos, end);
+            if (hit.isPresent()) {
+                double dist = eyePos.distanceTo(hit.get());
+                if (dist < closestDistance) {
+                    closestDistance = dist;
+                    entityHit = new EntityHitResult(entity, hit.get());
 
-                    }
                 }
             }
-            if (entityHit != null && entityHit.getEntity() != null) {
-                Entity target = entityHit.getEntity();
-                target.kill(serverLevel);
-            }
-
-
-
         }
-        player.playSound(SoundEvents.WARDEN_SONIC_BOOM, 8.0F, 15.0F);
+        if (entityHit != null && entityHit.getEntity() != null) {
+            Entity target = entityHit.getEntity();
+            target.kill(level);
+        }
+    }
 
-
-
-        return InteractionResult.FAIL;
+    private void spawnParticles(ServerLevel level, Player player) {
+        //particles
+        for (int i = 0; i < 100; i++) {
+            Vec3 scaled = player.getEyePosition().add(player.getLookAngle().scale(i));
+            level.sendParticles(ParticleTypes.SONIC_BOOM,scaled.x,scaled.y,scaled.z, 1,0,0,0,0);
+        }
     }
 
 
 
 
-
-
-    public void shoot() {
-
-    }
 }
