a7a5ea32a24d3f5973f263d87b8b625dc8524333
gr00ze <vincenzop23@gmail.com>
2025-10-18 01:30:24 +0200

    Split Animation

--- src/main/java/com/musketeers/foolish_guns/items/PrototypeGunItem.java
+++ src/main/java/com/musketeers/foolish_guns/items/PrototypeGunItem.java
@@ -1,10 +1,13 @@
 package com.musketeers.foolish_guns.items;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.*;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -29,11 +32,15 @@ import java.util.function.Consumer;
 
 public class PrototypeGunItem extends Item implements GeoItem {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    private static final RawAnimation ACTIVATE_ANIM = RawAnimation.begin().thenPlay("animation.gun_model.tesla");
+    private static final RawAnimation CHARGE_ANIMATION = RawAnimation.begin().thenPlay("animation.gun_model.tesla.charge");
+    private static final RawAnimation DISCHARGE_ANIMATION = RawAnimation.begin().thenPlay("animation.gun_model.tesla.discharge");
+    private static final String controllerName = "MyController";
     private boolean canFire = false;
     private boolean isLoading = false;
     private ServerLevel currentLevel;
     private Player currentPlayer;
+    private InteractionHand currentHand;
+    private int holdTime = 0;
 
     public PrototypeGunItem(Properties properties) {
 
@@ -49,8 +56,9 @@ public class PrototypeGunItem extends Item implements GeoItem {
         controllers.add(new AnimationController<>("Activation",0, animTest -> PlayState.STOP)
                 .triggerableAnim("activate", ACTIVATE_ANIM));
                 */
-        controllers.add(new AnimationController<>("MyController", 0, this::predicate)
-                .triggerableAnim("activate", ACTIVATE_ANIM)
+        controllers.add(new AnimationController<>(controllerName, 0, this::predicate)
+                .triggerableAnim("charge", CHARGE_ANIMATION)
+                .triggerableAnim("discharge", DISCHARGE_ANIMATION)
         );
 
     }
@@ -79,7 +87,10 @@ public class PrototypeGunItem extends Item implements GeoItem {
         this.spawnParticles(currentLevel,currentPlayer);
         this.hitEnemy(currentLevel,currentPlayer);
 
-        currentPlayer.playSound(SoundEvents.WARDEN_SONIC_BOOM, 8.0F, 15.0F);
+        //volume 0 - 1 - >1 distance
+        //pitch 0.5 - 1 - > 1 faster sound
+        currentLevel.playSound(null,currentPlayer.getX(),currentPlayer.getY(),currentPlayer.getZ(),SoundEvents.FIREWORK_ROCKET_BLAST, SoundSource.PLAYERS, 1F, 0.5F);
+        //currentPlayer.playSound(SoundEvents.WARDEN_SONIC_BOOM, 0.5F, 100F);
 
     }
 
@@ -96,25 +107,12 @@ public class PrototypeGunItem extends Item implements GeoItem {
     @Override
     @SuppressWarnings("unchecked")
     public void createGeoRenderer(Consumer consumer) {
-        System.out.println("Provider is "+ provider);
         if (provider == null)return;
         consumer.accept(provider);
 
     }
 
-    @Override
-    public boolean releaseUsing(ItemStack itemStack, Level level, LivingEntity livingEntity, int i) {
-        System.out.println("it is called");
-        onAnimationEnd();
-        AnimationController<?> controller = this.getAnimatableInstanceCache()
-                .getManagerForId(0)
-                .getAnimationControllers()
-                .get("MyController");
 
-        if (controller != null) controller.forceAnimationReset();
-        //player.getCooldowns().addCooldown(this, 40);
-        return super.releaseUsing(itemStack, level, livingEntity, i);
-    }
 
     @Override
     public @NotNull InteractionResult use(Level level, Player player, InteractionHand hand) {
@@ -123,18 +121,45 @@ public class PrototypeGunItem extends Item implements GeoItem {
 
         this.currentLevel = serverLevel;
         this.currentPlayer = player;
+        this.currentHand = hand;
 
         //animation
-        triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(hand), serverLevel), "MyController", "activate");
+        triggerAnim(player, GeoItem.getOrAssignId(player.getItemInHand(hand), serverLevel), controllerName, "charge");
 
         player.startUsingItem(hand);
         return InteractionResult.CONSUME;
     }
 
+    @Override
+    public boolean releaseUsing(ItemStack itemStack, Level level, LivingEntity livingEntity, int holdTime) {
+
+        onAnimationEnd();
+        this.holdTime = holdTime;
+
+
+        triggerAnim(currentPlayer, GeoItem.getOrAssignId(currentPlayer.getItemInHand(currentHand), currentLevel), controllerName, "discharge");
+        if (level instanceof ServerLevel serverLevel){
+            long id = GeoItem.getOrAssignId(itemStack, serverLevel);
+            AnimationController<?> controller = this.getAnimatableInstanceCache()
+                    .getManagerForId(id)
+                    .getAnimationControllers()
+                    .get(controllerName);
+
+            if (controller != null) {
+                //controller.forceAnimationReset();
+                //controller.setAnimationState(AnimationController.State.STOPPED);
+            }
+
+        }
+
+        //player.getCooldowns().addCooldown(this, 40);
+        return super.releaseUsing(itemStack, level, livingEntity, holdTime);
+    }
+
     private void hitEnemy(ServerLevel level, Player player) {
         Vec3 eyePos = player.getEyePosition(1.0F);
         Vec3 look = player.getViewVector(1.0F);
-        double range = 50.0;
+        double range = -holdTime;
         Vec3 end = eyePos.add(look.scale(range));
         EntityHitResult entityHit = null;
         double closestDistance = range;
@@ -154,13 +179,16 @@ public class PrototypeGunItem extends Item implements GeoItem {
         }
         if (entityHit != null && entityHit.getEntity() != null) {
             Entity target = entityHit.getEntity();
+            DamageSource ds = new DamageSource(Holder.direct(new DamageType("message id", 3)));
+            DamageSource ds2 = new DamageSource(Holder.direct(new DamageType("string", DamageScaling.ALWAYS, 1, DamageEffects.HURT, DeathMessageType.DEFAULT )));
+            //target.hurtServer(level, ds2, 2);
             target.kill(level);
         }
     }
 
     private void spawnParticles(ServerLevel level, Player player) {
         //particles
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < -holdTime; i++) {
             Vec3 scaled = player.getEyePosition().add(player.getLookAngle().scale(i));
             level.sendParticles(ParticleTypes.SONIC_BOOM,scaled.x,scaled.y,scaled.z, 1,0,0,0,0);
         }
