dea656342955c5ce71d0f5300ffc198bdf9ba348
gr00ze <vincenzop23@gmail.com>
2025-12-08 00:28:41 +0100

    Move on package

--- src/client/java/com/musketeers/foolish_guns/particle/ParticleList.java
+++ src/client/java/com/musketeers/foolish_guns/particle/ParticleList.java
@@ -1,6 +1,6 @@
 package com.musketeers.foolish_guns.particle;
 
-import com.musketeers.foolish_guns.GunParticles;
+import com.musketeers.foolish_guns.particles.GunParticles;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 
 
